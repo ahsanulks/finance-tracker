c1668fb2c045b1201bf0a465fdea18cb7ac66f09
ahsanulks <ahsanulkh996@gmail.com>
2025-02-23 00:41:24 +0300

    feat: implement transaction history retrieval

--- internal/usecase/transaction_history_report_usecase.go
+++ internal/usecase/transaction_history_report_usecase.go
@@ -2,15 +2,33 @@ package usecase
 
 import (
 	"context"
+	"financetracker/internal/entity"
 	"time"
 )
 
+type TransactionHistoryGetter interface {
+	FetchByPeriod(ctx context.Context, period time.Time) ([]*entity.Transaction, error)
+}
+
 type TransactionHistoryUsecase struct {
+	transactionHistoryGetter TransactionHistoryGetter
+}
+
+func NewTransactionHistoryUsecase(
+	transactionHistoryGetter TransactionHistoryGetter,
+) *TransactionHistoryUsecase {
+	return &TransactionHistoryUsecase{
+		transactionHistoryGetter: transactionHistoryGetter,
+	}
 }
 
 func (thu *TransactionHistoryUsecase) GenerateHistoryByDate(
 	ctx context.Context,
 	date time.Time,
 ) error {
+	_, err := thu.transactionHistoryGetter.FetchByPeriod(ctx, date)
+	if err != nil {
+		return err
+	}
 	return nil
 }
