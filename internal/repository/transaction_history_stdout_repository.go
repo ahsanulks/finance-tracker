cdfe59f3ad623a61b073f27c0d60d3bbfd6bf68b
ahsanulks <ahsanulkh996@gmail.com>
2025-02-23 22:46:46 +0300

    feat: implement writing transaction history to stdout

--- internal/repository/transaction_history_stdout_repository.go
+++ internal/repository/transaction_history_stdout_repository.go
@@ -2,7 +2,16 @@ package repository
 
 import (
 	"context"
+	"encoding/json"
 	"financetracker/internal/entity"
+	"fmt"
+	"strconv"
+	"time"
+)
+
+const (
+	dailyFormatDateReport   = "2006/01/02" // format: YYYY/MM/DD
+	monthlyFormatDateReport = "2006/01"    // format: YYYY/MM
 )
 
 type TransactionHistoryStdoutRepository struct{}
@@ -11,5 +20,53 @@ func (thsr *TransactionHistoryStdoutRepository) Write(
 	ctx context.Context,
 	transactionHistory *entity.TransactionHistory,
 ) error {
+	transactionReport := NewTransactionReport(transactionHistory)
+	transactionReportByte, _ := json.MarshalIndent(transactionReport, "", "  ")
+	fmt.Println(string(transactionReportByte))
 	return nil
+}
+
+type (
+	DailyDateReport   time.Time
+	MonthlyDateReport time.Time
+
+	TransactionReport struct {
+		Date         MonthlyDateReport `json:"period"`
+		TotalIncome  int64             `json:"total_income"`
+		TotalExpense int64             `json:"total_expense"`
+		Transactions []*Transaction    `json:"transactions"`
+	}
+
+	Transaction struct {
+		Date    DailyDateReport `json:"date"`
+		Amount  string          `json:"amount"`
+		Content string          `json:"content"`
+	}
+)
+
+func NewTransactionReport(transactionHistory *entity.TransactionHistory) *TransactionReport {
+	var transactions []*Transaction
+	for _, transaction := range transactionHistory.Transactions() {
+		transactions = append(transactions, &Transaction{
+			Date:    DailyDateReport(transaction.Date()),
+			Amount:  strconv.FormatInt(transaction.Amount(), 10),
+			Content: transaction.Content(),
+		})
+	}
+	return &TransactionReport{
+		Date:         MonthlyDateReport(time.Date(transactionHistory.YearPeriod(), time.Month(transactionHistory.MonthPeriod()), 1, 0, 0, 0, 0, time.Local)),
+		TotalIncome:  transactionHistory.TotalIncome(),
+		TotalExpense: transactionHistory.TotalExpenditure(),
+		Transactions: transactions,
+	}
+}
+
+func (ddr DailyDateReport) MarshalJSON() ([]byte, error) {
+	t := time.Time(ddr)
+	return json.Marshal(t.Format(dailyFormatDateReport))
+}
+
+func (mdr MonthlyDateReport) MarshalJSON() ([]byte, error) {
+	t := time.Time(mdr)
+	return json.Marshal(t.Format(monthlyFormatDateReport))
 }
