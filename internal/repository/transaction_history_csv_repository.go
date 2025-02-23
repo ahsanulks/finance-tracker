7e064d1e95a877107aca318c0b18449fd48a10b1
ahsanulks <ahsanulkh996@gmail.com>
2025-02-24 00:12:20 +0300

    feat: handle CSV file open failure

--- internal/repository/transaction_history_csv_repository.go
+++ internal/repository/transaction_history_csv_repository.go
@@ -3,14 +3,23 @@ package repository
 import (
 	"context"
 	"financetracker/internal/entity"
+	"os"
 )
 
 type TransactionHistoryCsvRepository struct {
+	filePath string
+}
+
+func NewTransactionHistoryCsvRepository(filePath string) *TransactionHistoryCsvRepository {
+	return &TransactionHistoryCsvRepository{
+		filePath: filePath,
+	}
 }
 
 func (thc *TransactionHistoryCsvRepository) FetchByPeriodDesc(
 	ctx context.Context,
 	period entity.TransactionPeriod,
 ) ([]*entity.Transaction, error) {
-	return nil, nil
+	_, err := os.Open(thc.filePath)
+	return nil, err
 }
