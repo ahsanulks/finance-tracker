dbaaf7f674c22b03d2fe50d9e4802842912cc6d1
ahsanulks <ahsanulkh996@gmail.com>
2025-02-25 01:14:05 +0300

    feat: add implementation check when file not found

--- internal/handler/transaction_history_report_cli_handler.go
+++ internal/handler/transaction_history_report_cli_handler.go
@@ -2,6 +2,8 @@ package handler
 
 import (
 	"errors"
+	"fmt"
+	"os"
 	"time"
 
 	"github.com/spf13/cobra"
@@ -36,5 +38,8 @@ func ValidateTransactionHistoryArgs(cmd *cobra.Command, args []string) error {
 }
 
 func ValidateCsvFileExist(cmd *cobra.Command, args []string) error {
+	if _, err := os.Stat(args[1]); os.IsNotExist(err) {
+		return fmt.Errorf("file not found: %s", args[1])
+	}
 	return nil
 }
